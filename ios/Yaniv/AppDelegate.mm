5a94780d0b5ea5d63101e2caaceb8471cd4cdbee
nbarkoch <naor.barkochva@bankleumi.co.il>
2025-06-28 22:22:47 +0300

    fix ios

--- ios/Yaniv/AppDelegate.mm
+++ ios/Yaniv/AppDelegate.mm
@@ -10,8 +10,6 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  [[RCTI18nUtil sharedInstance] allowRTL:NO];
-  [[RCTI18nUtil sharedInstance] forceRTL:NO];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
