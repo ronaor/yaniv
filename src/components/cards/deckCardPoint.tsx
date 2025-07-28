a8d644fff1dc89d58e266a13cf09d0d90714215d
nbarkoch <naor.barkochva@bankleumi.co.il>
2025-07-28 23:37:17 +0300

    refactor code of layers

--- src/components/cards/deckCardPoint.tsx
+++ src/components/cards/deckCardPoint.tsx
@@ -1,10 +1,11 @@
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, ViewStyle} from 'react-native';
 import React, {useEffect, useRef, useState} from 'react';
 import {Card, Position} from '~/types/cards';
 import {getCardKey, isCanPickupCard} from '~/utils/gameRules';
-import {DiscardedPointer, DiscardPointer, PickupPointer} from './discardPoint';
+import {DiscardedPointer, DiscardPointer} from './discardPoint';
 import {isEqual} from 'lodash';
 import {CARD_WIDTH} from '~/utils/constants';
+import PickupPointer from './pickupPoint';
 
 // This Component is responsible for managing the state of the discard pile.
 // it includes:
@@ -12,6 +13,15 @@ import {CARD_WIDTH} from '~/utils/constants';
 // 2. animate them when a new state come's up
 //    2.1. animate the cards that are moved to trash
 //    2.2. remove the card that is picked
+
+const cardsShifterStyle = (cardsLen: number): ViewStyle => ({
+  flexDirection: 'row',
+  transform: [
+    {
+      translateX: -((cardsLen - 1) * CARD_WIDTH) / 2,
+    },
+  ],
+});
 
 interface DeckCardPointersProps {
   cards: Card[];
@@ -75,102 +85,63 @@ const DeckCardPointers = ({
 
   return (
     <>
-      <>
-        <View
-          style={[
-            styles.body,
-            {
-              transform: [
-                {
-                  translateX:
-                    -((layerHistory.layer3.length - 1) * CARD_WIDTH) / 2,
-                },
-              ],
-            },
-          ]}
-          pointerEvents="none">
-          {layerHistory.layer3.map(
-            card =>
-              !card.picked && (
-                <DiscardedPointer
-                  card={card}
-                  throwTarget={{
-                    x: ((layerHistory.layer3.length - 1) * CARD_WIDTH) / 2,
-                    y: 2 * CARD_WIDTH,
-                    deg: card.deg,
-                  }}
-                  key={getCardKey(card)}
-                  opacity={{from: 0.4, to: 0.15}}
-                />
-              ),
-          )}
-        </View>
-        <View
-          style={[
-            styles.body,
-            {
-              transform: [
-                {
-                  translateX:
-                    -((layerHistory.layer2.length - 1) * CARD_WIDTH) / 2,
-                },
-              ],
-            },
-          ]}
-          pointerEvents="none">
-          {layerHistory.layer2.map(
-            card =>
-              !card.picked && (
-                <DiscardedPointer
-                  card={card}
-                  throwTarget={{
-                    x: ((layerHistory.layer2.length - 1) * CARD_WIDTH) / 2,
-                    y: 2 * CARD_WIDTH,
-                    deg: card.deg,
-                  }}
-                  key={getCardKey(card)}
-                  opacity={{from: 0.7, to: 0.4}}
-                />
-              ),
-          )}
-        </View>
-        <View
-          style={[
-            styles.body,
-            {
-              transform: [
-                {
-                  translateX:
-                    -((layerHistory.layer1.length - 1) * CARD_WIDTH) / 2,
-                },
-              ],
-            },
-          ]}>
-          {layerHistory.layer1.map(
-            (card, index) =>
-              !card.picked && (
-                <DiscardPointer
-                  card={card}
-                  index={index}
-                  throwTarget={{
-                    x: ((layerHistory.layer1.length - 1) * CARD_WIDTH) / 2,
-                    y: 2 * CARD_WIDTH,
-                    deg: card.deg,
-                  }}
-                  key={getCardKey(card)}
-                  opacity={0.7}
-                />
-              ),
-          )}
-        </View>
-      </>
       <View
-        style={[
-          styles.body,
-          {
-            transform: [{translateX: -((cards.length - 1) * CARD_WIDTH) / 2}],
-          },
-        ]}>
+        style={cardsShifterStyle(layerHistory.layer1.length)}
+        pointerEvents="none">
+        {layerHistory.layer3.map(
+          card =>
+            !card.picked && (
+              <DiscardedPointer
+                card={card}
+                throwTarget={{
+                  x: ((layerHistory.layer3.length - 1) * CARD_WIDTH) / 2,
+                  y: 2 * CARD_WIDTH,
+                  deg: card.deg,
+                }}
+                key={getCardKey(card)}
+                opacity={{from: 0.4, to: 0.15}}
+              />
+            ),
+        )}
+      </View>
+      <View
+        style={cardsShifterStyle(layerHistory.layer2.length)}
+        pointerEvents="none">
+        {layerHistory.layer2.map(
+          card =>
+            !card.picked && (
+              <DiscardedPointer
+                card={card}
+                throwTarget={{
+                  x: ((layerHistory.layer2.length - 1) * CARD_WIDTH) / 2,
+                  y: 2 * CARD_WIDTH,
+                  deg: card.deg,
+                }}
+                key={getCardKey(card)}
+                opacity={{from: 0.7, to: 0.4}}
+              />
+            ),
+        )}
+      </View>
+      <View style={cardsShifterStyle(layerHistory.layer1.length)}>
+        {layerHistory.layer1.map(
+          (card, index) =>
+            !card.picked && (
+              <DiscardPointer
+                card={card}
+                index={index}
+                throwTarget={{
+                  x: ((layerHistory.layer1.length - 1) * CARD_WIDTH) / 2,
+                  y: 2 * CARD_WIDTH,
+                  deg: card.deg,
+                }}
+                key={getCardKey(card)}
+                opacity={0.7}
+              />
+            ),
+        )}
+      </View>
+      <View style={cardsShifterStyle(cards.length)}>
         {cards.map((card, index) => (
           <PickupPointer
             disabled={disabled || !isCanPickupCard(cards.length, index)}
@@ -190,9 +161,6 @@ const DeckCardPointers = ({
 export default DeckCardPointers;
 
 const styles = StyleSheet.create({
-  body: {
-    flexDirection: 'row',
-  },
   discard: {
     width: CARD_WIDTH,
     height: 70,
---
+++ src/components/cards/pickupPoint.tsx
@@ -0,0 +1,66 @@
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+import {CardComponent} from './cardVisual';
+import {Pressable, StyleSheet} from 'react-native';
+import React, {useEffect} from 'react';
+import {Card, Position} from '~/types/cards';
+import {CARD_WIDTH, MOVE_DURATION} from '~/utils/constants';
+
+interface PickupPointerProps {
+  index: number;
+  card: Card;
+  fromTarget?: Position;
+  onPress: () => void;
+  disabled: boolean;
+}
+
+const PickupPointer = ({
+  index,
+  card,
+  fromTarget,
+  onPress,
+  disabled,
+}: PickupPointerProps) => {
+  const targetX = index * CARD_WIDTH;
+  const translateY = useSharedValue<number>(fromTarget?.y ?? 0);
+  const translateX = useSharedValue<number>(fromTarget?.x ?? targetX);
+  const cardDeg = useSharedValue<number>(fromTarget?.deg ?? 0);
+
+  useEffect(() => {
+    translateX.value = withTiming(targetX, {duration: MOVE_DURATION});
+    translateY.value = withTiming(0, {duration: MOVE_DURATION});
+    cardDeg.value = withTiming(0, {duration: MOVE_DURATION});
+  }, [translateX, translateY, cardDeg, card, targetX]);
+
+  const animatedPointerStyle = useAnimatedStyle(() => ({
+    transform: [{translateX: translateX.value}],
+  }));
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [
+      {translateY: translateY.value},
+      {rotate: `${cardDeg.value}deg`},
+    ],
+  }));
+
+  return (
+    <Animated.View style={[styles.pointers, animatedPointerStyle]}>
+      <Animated.View style={animatedStyle}>
+        <Pressable disabled={disabled} onPress={onPress}>
+          <CardComponent card={card} />
+        </Pressable>
+      </Animated.View>
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  pointers: {
+    position: 'absolute',
+  },
+});
+
+export default PickupPointer;
