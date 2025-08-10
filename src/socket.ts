5839af02cda44ec24f297d1a0b9488ed5a6aaefe
nbarkoch <naor.barkochva@bankleumi.co.il>
2025-08-11 00:07:13 +0300

    fix socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,5 @@
 import {io, Socket} from 'socket.io-client';
-// import {SERVER_URL} from '@env';
-const SERVER_URL = 'http://10.100.102.11:3000/';
+import {SERVER_URL} from '@env';
 
 const serverUrl = SERVER_URL ?? 'https://yaniv-backend.onrender.com';
 
